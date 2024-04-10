05ab099a5914ba5005ab018d1f41718e54cb0033
Sivakumar <balusiva2312@gmail.com>
2024-04-10 19:15:27 +0530

    first commit6

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 console.log("BACKEND_URL:", process.env.BACKEND_URL); // Debug statement
 const instance = axios.create({
     //baseURL: `${process.env.BACKEND_URL}api` || 'http://localhost:4000/api'
-    baseURL: 'https://react-hrms-um31.onrender.com/api'
+    baseURL: 'http://localhost:4000/api'
 })
 
 export default instance
